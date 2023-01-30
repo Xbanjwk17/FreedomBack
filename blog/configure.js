b4c98945ff1dee50015b6b0be03c9eb1f5e4a73a
Xbanjwk <123902772+Xbanjwk17@users.noreply.github.com>
2023-01-30 13:02:06 -0300

    Update configure.js

--- blog/configure.js
+++ blog/configure.js
@@ -1,5 +1,5 @@
 function goTo() {
-    let goToPage = prompt("Go to:", "year/month/day")
+    let goToPage = prompt("Go to:", "year/month/day/name.html")
 
     document.URL = "https://xbanjwk17.github.io/FreedomBack/blog/" + goToPage
 }
